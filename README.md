e7c26b05fe09e84d8c0b2f05b2662a6f102d8826
Mayur <6332663+mayurjobanputra@users.noreply.github.com>
2024-01-02 13:55:56 -0800

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Personas-Picker-WordPress-Plugin
-The Persona Picker Plugin for WordPress effortlessly creates and manages a custom 'Persona' post type, enhancing content organization and interactivity on your website.
+# Persona Picker Plugin for WordPress
+
+The Persona Picker Plugin effortlessly creates and manages a custom 'Persona' post type, enhancing content organization and interactivity on your website.
+
+## Description
+
+This plugin allows WordPress users to create and manage a new post type called 'Personas'. It includes standard WordPress fields such as title, content, categories, and tags. A unique feature of this plugin is the ability to output the titles of these Persona posts in a specific format using a shortcode, making it ideal for users who want to enhance their website's interactivity and organization.
+
+## Installation
+
+1. **Download the Plugin:**
+   - Download the zip file of the plugin from GitHub.
+   - Unzip the file into your `wp-content/plugins` directory.
+
+2. **Activate the Plugin:**
+   - Navigate to the 'Plugins' menu in WordPress.
+   - Locate 'Persona Picker Plugin' and click 'Activate'.
+
+3. **Using the Plugin:**
+   - Once activated, the plugin automatically creates the 'Persona' custom post type.
+   - You can start adding new Persona posts from the 'Personas' menu in the WordPress admin area.
+
+## Usage
+
+- **Creating Personas:**
+  - Go to 'Personas' -> 'Add New Persona' in your WordPress dashboard to add new Persona posts.
+- **Displaying Personas:**
+  - Use the shortcode `[persona_picker]` in your posts or pages to display the Persona titles in the specified format.
+
+## Contributing
+
+I am always looking for ways to improve this plugin. If you have suggestions or enhancements, feel free to fork this repository and submit a pull request. Your contributions are greatly appreciated!
+
+## Support
+
+For support, please open an issue on the GitHub repository. I'll do my best to address it as quickly as possible.
+
+## License
+
+This plugin is released under the [GPLv2 license](https://www.gnu.org/licenses/gpl-2.0.html).
+
+Remember, if you appreciate the assistance, you can leave a tip at https://howtobuildwordpress.com/gptbot.
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+/*
+Plugin Name: Persona Picker Plugin
+Author: Mayur Jobanputra
+Version: 1.0
+*/
+
+// Check if the post type already exists and if /you-are/ slug is being used
+function persona_picker_activate() {
+    if (post_type_exists('persona')) {
+        die('Persona post type already exists. Plugin activation stopped.');
+    }
+
+    $slug = 'you-are';
+    $slugExists = persona_picker_slug_exists($slug);
+    
+    if ($slugExists) {
+        $slug = 'you-are-user';
+        $slugExists = persona_picker_slug_exists($slug);
+    }
+
+    if ($slugExists) {
+        $slug = 'you-are-user-role';
+        $slugExists = persona_picker_slug_exists($slug);
+    }
+
+    if ($slugExists) {
+        die('All potential slugs for Persona post type are already in use. Plugin activation stopped.');
+    }
+
+    create_persona_post_type($slug);
+    flush_rewrite_rules();
+}
+
+
+register_activation_hook(__FILE__, 'persona_picker_activate');
+
+// Register Custom Post Type with dynamic slug
+function create_persona_post_type($slug = 'you-are') {
+    register_post_type('persona', array(
+        'labels' => array('name' => __('Personas'), 'singular_name' => __('Persona')),
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'categories', 'tags'),
+        'rewrite' => array('slug' => $slug),
+    ));
+}
+
+// Check if a given slug already exists in WordPress
+function persona_picker_slug_exists($slug) {
+    global $wpdb;
+    if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $slug . "'", 'ARRAY_A')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+add_action('init', 'create_persona_post_type');
+
+// Shortcode to display Persona titles
+function persona_picker_shortcode() {
+    $query = new WP_Query(array('post_type' => 'persona'));
+    $output = '';
+
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_title = get_the_title();
+        $slug_title = sanitize_title($post_title);
+        $output .= "<span class='persona-choice'><a href='#tab-" . $slug_title . "'>" . $post_title . "</a></span>";
+    }
+
+    wp_reset_postdata();
+    return $output;
+}
+
+add_shortcode('persona_picker', 'persona_picker_shortcode');
+
+// Add menu items
+function add_persona_menu_items() {
+    add_menu_page('Personas', 'Personas', 'manage_options', 'edit.php?post_type=persona', '', 'dashicons-groups', 20);
+    add_submenu_page('edit.php?post_type=persona', 'Add New Persona', 'Add New Persona', 'manage_options', 'post-new.php?post_type=persona');
+    add_submenu_page('edit.php?post_type=persona', 'Categories', 'Categories', 'manage_options', 'edit-tags.php?taxonomy=category&post_type=persona');
+    add_submenu_page('edit.php?post_type=persona', 'Tags', 'Tags', 'manage_options', 'edit-tags.php?taxonomy=post_tag&post_type=persona');
+    add_submenu_page('edit.php?post_type=persona', 'Persona Shortcodes', 'Persona Shortcodes', 'manage_options', 'persona_shortcodes', 'persona_shortcodes_page');
+}
+
+add_action('admin_menu', 'add_persona_menu_items');
+
+function persona_shortcodes_page() {
+    echo '<div class="wrap"><h1>Persona Shortcodes</h1><p>Use the shortcode [persona_picker] to display Persona titles.</p></div>';
+}
+
+// Flush rewrite rules on deactivation
+function persona_picker_deactivate() {
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook(__FILE__, 'persona_picker_deactivate');
